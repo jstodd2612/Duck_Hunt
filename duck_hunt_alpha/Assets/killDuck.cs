14a8c969c2e9dae80e6f27a7b939db2a0805624b
Micah Schow <micah.schow@gmail.com>
2014-12-01 16:42:58 -0700

    Random movement improved, duck death (including state machine) added.

---
+++ duck_hunt_alpha/Assets/killDuck.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killDuck : MonoBehaviour {
+
+	static public bool isDead = false;
+	private Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator>();
+		anim.SetBool("isDead", false);
+	}
+
+	void OnMouseDown(){
+		isDead = true;
+		anim.SetBool("isDead", true);
+	}
+}
