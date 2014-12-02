98b2771672665bc4e995157c300630dc4f52fe7f
Tanner Schreyer <tannerschreyer@tanners-mbp.ad.uvu.edu>
2014-12-02 16:02:43 -0700

    Duck Hunt Game upload

---
+++ DuckHuntGame/Assets/BirdScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BirdScript: MonoBehaviour {
+
+public int score;
+public Text scoreText;
+	public GameObject Bird;
+
+void Start() {
+	score = 0;
+	UpdateScore ();
+
+}
+
+
+	void OnMouseDown () {
+		//Destroy (gameObject);
+		score += 10;
+		UpdateScore ();
+		Bird.SetActive (false);
+	}
+
+
+
+void UpdateScore(){
+	scoreText.text = "Score:" + score;
+}
+}
