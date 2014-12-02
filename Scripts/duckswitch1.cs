0742af1850a49ff645cabbd5ec0ac97115f76292
Taylor Mumford <Shadowfox.11@hotmail.com>
2014-12-02 15:37:00 -0700

    These are all my files, the ducks fly, die, and there is a place for the score that doesn't work. I am trying to have the duck dying animation happen when you click the live duck but i don't know how.

---
+++ Scripts/duckswitch1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class duckswitch1 : MonoBehaviour {
+	public Animation deadduck;
+	
+	void OnMouseDown () {
+		Destroy (gameObject);
+		animation.Play ("deadduck");//doesn't work
+	}
+}
+
+/*I can't figure out how to make the dead duck animation show up after you click the duck*/
