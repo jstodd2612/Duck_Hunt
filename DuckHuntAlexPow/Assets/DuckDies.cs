407f6e98f77df9d574fc50b103cd0d6f5ea7a0cf
Alexandra Powelson <alexpow8@gmail.com>
2014-12-02 15:28:11 -0700

    no message

---
+++ DuckHuntAlexPow/Assets/DuckDies.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckDies : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+
+	}
+	void onMouseDown(){
+		Destroy (gameObject);
+
+	}
+	//when duck hits collider, destroy. so, top collider and bottom collider. it falls when clicked, then dies when hit bottom collider 
+}
