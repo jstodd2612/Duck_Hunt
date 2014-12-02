ce661293678b5aa73b6b744d49387c2d263567f2
Andrew Jensen <navysealboy26@hotmail.com>
2014-12-01 19:09:48 -0700

    This is the basic file. Includes: Background Sprite DuckDie script DuckFly script For your info, merely for testing purposes, in the duckdie script I used spacebar as a way to kill the duck. Obviously this all needs fixing, but this is a start.

---
+++ Assets/scripts/movement/duckFly.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class duckFly : MonoBehaviour {
+
+	public float moveSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int RandomNum = Random.Range(1,4);
+
+		switch(RandomNum){ //Still need to add time to the random movement.
+		 //Otherwise the movement is only for a millisecond in one direction.
+		case 1:
+			transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+			break;
+
+		case 2:
+			transform.Translate(-Vector2.up * moveSpeed * Time.deltaTime);
+			break;
+
+		case 3:
+			transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+			break;
+
+		case 4:
+			transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+			break;
+		}
+	}
+}
