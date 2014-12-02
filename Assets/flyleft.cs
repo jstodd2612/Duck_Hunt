73a882855f230055513c087c6eb7b8f7f473c792
Brittany <brittonyas@aol.com>
2014-12-02 10:29:33 -0700

    Working on images

---
+++ Assets/flyleft.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flyleft : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
