321c0a85bf383b7916ece4c2a3ccfc124fdab59d
Andrew Jensen <navysealboy26@hotmail.com>
2014-12-03 18:14:25 -0700

    Added state drive to dog, Score GUIText, continued to troubleshoot random direction movement in duckFly script. Also added two animations to dog, 1. jump around for hit 2. lay face down for miss.

--- Assets/scripts/movement/duckFly.cs
+++ Assets/scripts/movement/duckFly.cs
@@ -5,33 +5,40 @@ public class duckFly : MonoBehaviour {
 
 	public float moveSpeed = 3f;
 
-	// Use this for initialization
-	void Start () {
-	
+	void FlyRandom () {
+
+		int RandomNum = Random.Range(1,4);
+		
+		switch(RandomNum){ //Still need to add time to the random movement.
+			//Otherwise the movement is only for a millisecond in one direction.
+		case 1:
+			transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+			break;
+			
+		case 2:
+			transform.Translate(-Vector2.up * moveSpeed * Time.deltaTime);
+			break;
+			
+		case 3:
+			transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+			break;
+			
+		case 4:
+			transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+			break;
+		}
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
 
-		int RandomNum = Random.Range(1,4);
-
-		switch(RandomNum){ //Still need to add time to the random movement.
-		 //Otherwise the movement is only for a millisecond in one direction.
-		case 1:
-			transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
-			break;
-
-		case 2:
-			transform.Translate(-Vector2.up * moveSpeed * Time.deltaTime);
-			break;
-
-		case 3:
-			transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-			break;
-
-		case 4:
-			transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
-			break;
+		void IntervalFly(){
+			
+			int RandomFlyLength = Random.Range(2,4);
+			int RandomFlyInterval = Random.Range(1f,3f):
+			
+			InvokeRepeating("FlyRandom", "RandomFlyLength", "RandomFlyInterval");
 		}
 	}
 }
