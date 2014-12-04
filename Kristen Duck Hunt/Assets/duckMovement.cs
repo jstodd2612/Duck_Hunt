f6c583c6be3b97758e261bc7ad53796b4373159b
kdlund <jaredlund@Jared-Lunds-MacBook-Pro.local>
2014-12-04 00:14:39 -0700

    duck flys, duck dies, function GUI, dog state machine, duck changes direction

---
+++ Kristen Duck Hunt/Assets/duckMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class duckMovement : MonoBehaviour {
+
+	public float Xmax = 4.5f;
+	public float Xmin = -4.5f;
+	public float Ymin = -1.5f;
+	public float Ymax = 2.7f;
+	public int moveSpeed = 1;
+
+	private float firstDirection = 0.0f;
+	private float Xrandom;
+	private float Yrandom;
+
+	void Update(){
+		if (killDuck.isDead == false){
+			randomMovement();
+			} 
+		/*else {
+			transform.Translate(new Vector3(Xrandom, -2.0f, 0.0f) * Time.deltaTime);
+		}*/
+	}
+
+	void randomMovement() {
+		if(Time.deltaTime >= firstDirection){
+
+			Xrandom = Random.Range (-2.0f, 2.0f);
+			Yrandom = Random.Range (-2.0f, 2.0f);
+
+			firstDirection = Time.deltaTime + Random.Range (0.5f, 1.5f);
+			}
+
+		transform.Translate(new Vector3(Xrandom,Yrandom,0.0f) * moveSpeed * Time.deltaTime);
+
+		if (transform.position.x >= Xmax || transform.position.x <= Xmin) {
+			Xrandom = -Xrandom;
+		}
+		if (transform.position.y >= Ymax || transform.position.y <= Ymin) {
+			Yrandom = -Yrandom;
+		}
+
+		if (shots.gunshots <= 0){
+			Yrandom = 10;
+		}
+}
+}
---
+++ Kristen Duck Hunt/Assets/killDuck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killDuck : MonoBehaviour {
+
+	static public bool isDead = false;
+	static public int scores = 0;
+	private Animator anim;
+	public const int scoreIncrease = 1000;
+
+
+	void Start(){
+		anim = GetComponent<Animator>();
+		anim.SetBool("isDead", false);
+	}
+
+	void OnMouseDown(){
+		isDead = true;
+		anim.SetBool("isDead", true);
+		scores = scores + scoreIncrease;
+		shots.gunshots = shots.gunshots - 1;
+	}
+
+	void resetStage(){
+		isDead = false;
+	}
+	static public void flyAway(){
+
+	}
+}
