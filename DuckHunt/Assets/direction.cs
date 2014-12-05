40243373b5c22d9db80514ae0d4aadc3ad9d3dac
Michael Davis <michaelaustindavis@gmail.com>
2014-12-04 19:43:59 -0700

    DuckHunt

---
+++ DuckHunt/Assets/direction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class direction : MonoBehaviour 
+{
+
+	public float speed = .03f;
+	public bool birddirection = false;
+	
+	void Start () {
+		birddirection = (Random.value < .5);
+	}
+	
+	void Update () {
+		if (birddirection) {
+			transform.position += new Vector3 (speed, speed);
+		} 
+		else{
+			transform.position += new Vector3 (speed * -1, speed);
+		}
+	}
+}
