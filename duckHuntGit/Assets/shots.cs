cd326376c58158444a53e166ac824176857a04e6
Andrew Jensen <navysealboy26@hotmail.com>
2014-12-11 14:44:41 -0700

    Duck GUI deactivates the last of the kill duck gui sprites.

--- duckHuntGit/Assets/shots.cs
+++ duckHuntGit/Assets/shots.cs
@@ -7,24 +7,17 @@ public class shots : MonoBehaviour {
 	//public List<GameObject> bullets = new List<GameObject>();
 	public GameObject shotOn;
 	public GameObject[] guiDuck;
-	//public static GameObject[] (string "duckGUIhit");
+
 
 	void Start(){
 		//Physics.IgnoreCollision();
-
-		//if (guiDuck == null)
 			guiDuck = GameObject.FindGameObjectsWithTag ("duckGUIhit");
-
-		Debug.Log ("Gui duck 1: " + guiDuck [0]);
-		Debug.Log ("Gui duck 2: " + guiDuck [1]);
-
 	}
 	void OnMouseDown(){
 		if (StaticVars.bullets > 0 && StaticVars.duckIsDead != true) { 
 			StaticVars.bullets = StaticVars.bullets - 1;
-			//bullets[0].SetActive(false); //this needs to be attached to the array when an element is removed
-			//bullets.RemoveAt(0);
 			BulletSprites();
+			DuckGUIHit();
 		}
 
 	}
@@ -41,7 +34,7 @@ public class shots : MonoBehaviour {
 
 
 	public void DuckGUIHit (){
-		//guiDuck[0] = GameObject.SetActive(false);
+		guiDuck[0].SetActive(false);
 		//guiDuck.Remove;
 		}
 
