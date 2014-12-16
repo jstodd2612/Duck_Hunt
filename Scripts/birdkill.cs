2bdd7a5a0aace564a6de1d506cd52df100e28f1d
Taylor Mumford <Shadowfox.11@hotmail.com>
2014-12-16 11:42:05 -0700

    BAM!!! It all works.

--- Scripts/birdkill.cs
+++ Scripts/birdkill.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class birdkill : MonoBehaviour {
 
 	public GameObject bird;
-	// Use this for initialization
 	void OnTriggerEnter2D (){
 		Object.Destroy (bird);
 
