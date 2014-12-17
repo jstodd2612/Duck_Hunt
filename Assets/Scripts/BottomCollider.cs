18f41921c94eb1d584b7e5a08630e9e1fa1d5f1e
Laura Adamson <adamsn14@aol.com>
2014-12-16 18:20:58 -0700

    Replaced all the old broken files with the new

---
+++ Assets/Scripts/BottomCollider.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomCollider : MonoBehaviour {
+	
+	public Animator Dog;
+	public GameObject nextButton;
+	private float disableAnimation;
+	
+	public void OnTriggerEnter2D (Collider2D _c){
+		GameObject duck = Object.Instantiate (_c.gameObject) as GameObject;
+		flight f = duck.GetComponent("flight") as flight;
+		f.isAlive = true;
+		duck.rigidbody2D.transform.position = new Vector3(.5f, -1f, 0f);
+		StaticVars.shouldSpawnNewDuck = false;
+		
+		
+		Object.Destroy (_c.gameObject);
+		Dog.SetBool ("hit", true);
+		disableAnimation = Time.time + 1f;
+		StaticVars.shouldSpawnNewDuck = true;
+		
+		
+		if (StaticVars.bullets <= 0) {
+			StaticVars.level++;
+			StaticVars.bullets = 10;
+		}
+
+		StaticVars.ducksKilled++;
+		//StaticVars.shouldSpawnNewDuck = true;
+	}
+
+	void Update() {
+		if (disableAnimation < Time.time)
+						Dog.SetBool ("hit", false);
+
+	}
+}
---
+++ Assets/Scripts/topCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class topCollider : MonoBehaviour {
+	
+	public Animator Dog;
+	private float animationTimer;
+	
+	public void OnTriggerEnter2D (Collider2D _c){
+		GameObject duck = Object.Instantiate (_c.gameObject) as GameObject;
+		flight f = duck.GetComponent("flight") as flight;
+		f.isAlive = true;
+		duck.rigidbody2D.transform.position = new Vector3(.5f, -1f, 0f);
+		StaticVars.shouldSpawnNewDuck = false;
+		
+		
+		Object.Destroy (_c.gameObject);
+		Dog.SetBool ("miss", true);
+		animationTimer = Time.time + 1f;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (animationTimer < Time.time)
+						Dog.SetBool ("miss", false);
+	}
+}
